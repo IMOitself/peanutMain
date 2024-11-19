31a06f5c08591ac124464f3327135c489de884dc
IMOitself (github action) <russjonn@gmail.com>
2024-11-19 01:42:04 +0000

    feat: draw text on view using Canvas
     (auto sync)

--- java/imo/peanut/MainActivity.java
+++ java/imo/peanut/MainActivity.java
@@ -1,7 +1,12 @@
 package imo.peanut;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +15,31 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        View view = findViewById(R.id.view);
+
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                drawCanvasOnView(view);
+           }
+        });
+    }
+
+
+    void drawCanvasOnView(View view){
+        int width = view.getWidth();
+        int height = view.getHeight();
+        int textSize = height / 8;
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setTextSize(textSize);
+        paint.setColor(0xFFFFFFFF);
+
+        canvas.drawText("UwU", 0, textSize, paint);
+
+        view.setBackground(new BitmapDrawable(getResources(), bitmap));
     }
 }
